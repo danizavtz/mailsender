74a03774eaa7c70c3ecb308980d24e2648b8d12f
Danizavtz <daninove@gmail.com>
2020-06-25 00:19:24 -0300

    Atualização de versoes de libs

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.disable('x-powered-by');
 cors({ credentials: true, origin: true });
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json({ type: 'application/json' }));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(require('./server/index'));
 module.exports = app;
