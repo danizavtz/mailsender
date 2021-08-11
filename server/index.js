eac1c574c6565bc281223a7d71d9b9767705e024
danizavtz <daniellucena@yahoo.com.br>
2021-08-11 10:41:18 -0300

    improvements in application flow, new error format for not found path, lib version bumps

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 router.use(require('./routes/mail.route'));
 
 router.get('/', (req, res) => {
-    res.status(200).json({msg: "server up and running"})
+    req.servermsg = { msg: "server up and running" }
+    res.status(200).json(req.servermsg)
 });
 router.get('*', (req, res) => {
-    res.status(404).json('Não encontrado');
+    res.status(404).json({ errors: [{location: req.path, msg: 'Not found', param: null}]});
 });
 
 
